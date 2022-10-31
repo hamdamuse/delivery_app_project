eed5d62e46b09d0aa62851644afefab73250c76e
Hamda Muse <hmuse300@gmail.com>
2022-10-31 16:28:35 +0000

    updating files

--- src/app.py
+++ src/app.py
@@ -20,8 +20,6 @@ Update product/courier list: 3
 Delete product/courier: 4:
 """))
 
-    if option < 0 or option > 4:
-        raise ValueError("Please enter a number between 0 and 4.")
     while option != 0:
         if option ==1:
             print_menu()
@@ -31,6 +29,8 @@ Delete product/courier: 4:
             update_item()
         elif option == 4:
             delete_item()
+        else:
+            print("Please enter a number between 0 and 4.")
 
 def print_menu():
     which_file = int(input("""
@@ -40,17 +40,17 @@ Courier: 2
 """))
     if which_file == 1:
         try:
-            with open(r"mini project products list.txt", "r") as product:
+            with open(r"data\products.txt", "r") as product:
                 product_list = product.readlines()
                 formatted_product_list = [i.strip("\n") for i in product_list]
                 print(f"Products: {formatted_product_list}")
         except Exception as e:
             print(f"Error: {str(e)}.")
-            os.system("clear")
+            
         return sub_menu()
     elif which_file == 2:
         try:
-            with open(r"mini project courier list.txt", "r") as courier:
+            with open(r"data\couriers.txt", "r") as courier:
                 courier_list = courier.readlines()
                 formatted_courier_list = [i.strip("\n") for i in courier_list]
                 print(f"Couriers: {formatted_courier_list}")
@@ -65,21 +65,131 @@ Product: 1
 Courier: 2
 """))
     if which_file == 1:
-        new_item = input("Enter new product:\n")
+        new_item = input("Enter new product:\n").lower()
         try:
-            with open(r"mini project products list.txt","a") as products:
+            with open(r"data\products.txt","a") as products:
                 products.write("\n" + new_item)
         except Exception as e:
             print(f"Error: {str(e)}")
         return sub_menu()
     elif which_file == 2:
-        new_item = input("Enter new courier:\n")
+        new_item = input("Enter new courier:\n").lower()
         try:
-            with open(r"mini project courier list.txt","a") as courier:
-                courier.write("\n"+ new_item)
+            with open(r"data\couriers.txt","a") as courier:
+                courier.write("\n" + new_item)
         except Exception as e:
             print(f"Error: {str(e)}")
         return sub_menu()
+
+def update_item():
+    which_file =  int(input("""
+Which menu would you like to access?
+Product: 1
+Courier: 2
+"""))
+    if which_file == 1:
+        
+        try:
+            with(open(r"data\products.txt", "r")) as f:
+                file_lines = f.readlines()
+                formatted_product_list = [i.strip("\n") for i in file_lines]
+            print(formatted_product_list)
+            index_to_update =int(input(f"""
+The product list items with their index items are : {list(enumerate(formatted_product_list))}.
+Please enter the index of the item you would like to update.
+"""))
+            updated_product = input("Please enter the new product name.\n")
+            for item in range(len(formatted_product_list)):
+                if item == index_to_update:
+                    formatted_product_list[index_to_update] = updated_product
+        except Exception as e:
+            print(f"Error: {str(e)}")
+
+        try:
+            with open(r"data\products.txt","w") as f:
+                for i in formatted_product_list:
+                    f.write(i+"\n")
+        except Exception as e:
+            print(f"Error: {str(e)}")
+
+    elif which_file == 2:
+        try:
+            with(open(r"data\couriers.txt", "r")) as f:
+                file_lines = f.readlines()
+                formatted_courier_list = [i.strip("\n") for i in file_lines]
+                index_to_update =int(input(f"""
+The product list items with their index items are : {list(enumerate(formatted_courier_list))}.
+Please enter the index of the item you would like to update.
+"""))
+                updated_courier = input("Please enter the new courier name.\n").title()
+                for item in range(len(formatted_courier_list)):
+                    if item == index_to_update:
+                        formatted_courier_list[index_to_update] = updated_courier
+        except Exception as e:
+            print(f"Error: {str(e)}")
+
+        try:
+            with open(r"data\couriers.txt","w") as f:
+                for i in formatted_courier_list:
+                    f.write(i+"\n")
+        except Exception as e:
+            print(f"Error: {str(e)}")
+    return sub_menu()
+
+def delete_item():
+    which_file =  int(input("""
+Which menu would you like to access?
+Product: 1
+Courier: 2
+"""))
+    if which_file == 1:
+        try:
+            with open(r"data\products.txt", "r") as f:
+                formatted_products = [i.strip("\n") for i in f.readlines()]
+            print(formatted_products)
+            index_to_delete =int(input(f"""
+The product list items with their index items are : {list(enumerate(formatted_products))}.
+Please enter the index of the item you would like to delete.
+"""))
+        except Exception as e:
+            print(f"Error: {str(e)}")
+        for index in range(len(formatted_products)):
+            if index == index_to_delete:
+                formatted_products.remove(formatted_products[index])
+        print(formatted_products)
+        try:
+            with open(r"data\products.txt","w") as f:
+                for i in formatted_products:
+                    f.write(i+"\n")
+        except Exception as e:
+            print(f"Error: {str(e)}")
+    elif which_file == 2:
+        try:
+            with open(r"data\couriers.txt", "r") as f:
+                formatted_couriers = [i.strip("\n") for i in f.readlines()]
+            print(formatted_couriers)
+            index_to_delete =int(input(f"""
+The product list items with their index items are : {list(enumerate(formatted_couriers))}.
+Please enter the index of the item you would like to delete.
+"""))
+        except Exception as e:
+            print(f"Error: {str(e)}")
+        for index in range(len(formatted_couriers)):
+            if index == index_to_delete:
+                formatted_couriers.remove(formatted_couriers[index])
+        print(formatted_couriers)
+        try:
+            with open(r"data\couriers.txt","w") as f:
+                for i in formatted_couriers:
+                    f.write(i+"\n")
+        except Exception as e:
+            print(f"Error: {str(e)}")
+    return sub_menu()
+
+    
+        
+    
+
 
 if __name__ == "__main__":
     main_menu()
