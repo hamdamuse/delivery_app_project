022df3d558e0efacdd8a97033b1b6a054c7fbe8e
Hamda Muse <hmuse300@gmail.com>
2022-11-22 21:48:33 +0000

    updated UI layer
    UI layer now is linked to model for the following functions: print_products, print_couriers, print_orders, add_courier, add_product

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ def navigate_product_menu():
                 # include save property
                 navigate_main_menu()
             elif cmd == 1:
-                model.print_products()
+                model.print_products(model.product_list)
             elif cmd == 2:
                 product_name = input("Enter new product name -> ")
                 product_price = float(input("Enter new product price -> "))
@@ -108,12 +108,13 @@ def navigate_courier_menu():
                 # include save property
                 navigate_main_menu()
             elif cmd == 1:
-                print("Print courier pending")
+                model.print_couriers(model.courier_list)
                 break
             elif cmd == 2:
-                print("Add courier pending")
-                break
-            elif cmd == 3:
+                courier_name = input("Please enter courier name -> ")
+                courier_phone = input("Please enter courier phone number -> ")
+                model.add_courier(model.courier_list, courier_name, courier_phone)
+            elif cmd == 3: 
                 print("Update courier pending")
                 break
             elif cmd == 4:
@@ -128,7 +129,41 @@ def navigate_courier_menu():
 
 
 def print_order_menu_options():
-    pass
+    print("""
+    ORDER MENU
+    Exit to MAIN MENU: 0
+    Print orders: 1
+    Add order: 2
+    Update order: 3
+    """)
+
+def navigate_order_menu():
+    while True:
+        print_order_menu_options()
+        try:
+            cmd = int(input("Enter input here -> "))
+            if cmd == 0:
+                navigate_main_menu()
+            elif cmd == 1:
+                model.print_orders(model.order_list)
+                break
+            elif cmd == 2:
+                customer_name = input("Please enter customer name -> ")
+                customer_address = input("Please enter customer phone number -> ")
+                customer_phone_number = input("Please enter customer phone number ->")
+                model.add_order(model.order_list, customer_name, customer_address, customer_phone_number)
+            elif cmd == 3: 
+                print("Update courier pending")
+                break
+            elif cmd == 4:
+                print("Delete courier pending")
+                break
+            else:
+                print("Error: please input valid number.")
+            continue
+        except ValueError:
+            print("Error: please enter a number")
+            continue
 
 
 if __name__ == "__main__":
