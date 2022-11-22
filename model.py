cf3947e09dadbe2e295fc8bc26bc623a794e4462
Hamda Muse <hmuse300@gmail.com>
2022-11-22 19:50:03 +0000

    had to move to root directory
    couldn't get both pytest and python to work with the imports, it's a quick fix but I had no other options

---
+++ model.py
@@ -0,0 +1,53 @@
+
+from csv_layer import load,products_file_path,couriers_file_path,orders_file_path,save_couriers,save_orders,save_products
+
+
+def load_products():
+    product_list = load(products_file_path)
+    formatted_product_list = [{"name": product["name"], "price": float(product['price'])} for product in product_list]
+    return formatted_product_list
+
+product_list = load_products()
+
+def load_couriers():
+    courier_list = load(couriers_file_path)
+    #formatted_courier_list = [{}]
+    return courier_list
+
+courier_list = load_couriers()
+
+
+def load_orders():
+    order_list = load(orders_file_path)
+    return order_list
+
+order_list = load_orders()
+
+def save_products_to_csv():
+    save_products(product_list)
+
+def save_couriers_to_csv():
+    save_couriers(courier_list)
+
+def save_orders_to_csv():
+    save_orders(order_list)
+
+def print_products(product_list):
+    for product in product_list:
+        print(product_list)
+
+
+def add_product(product_list, name, price):
+    new_product= {'name': name, 'price': price}
+    product_list.append(new_product)
+    return product_list
+
+def add_courier(courier_list, name, phone):
+    new_courier = {'name': name, 'phone_number': phone}
+    courier_list.append(new_courier)
+    return courier_list
+
+def add_order(order_list):
+    pass
+
+print_products(product_list)
