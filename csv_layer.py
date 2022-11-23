4992f2356f07ac25cf976456a509623ec7c759b8
Hamda Muse <hmuse300@gmail.com>
2022-11-23 00:03:32 +0000

    removed bug that made it such that only one row of files were loaded into memory

--- csv_layer.py
+++ csv_layer.py
@@ -1,9 +1,9 @@
-from pathlib import Path
 import csv
 
 products_file_path = r"data\products.csv"
 couriers_file_path = r"data\couriers.csv"
 orders_file_path = r"data\orders.csv"
+
 
 def load(file_name):
     item_list = []
@@ -12,7 +12,7 @@ def load(file_name):
             csv_file = csv.DictReader(file)
             for row in csv_file:
                 item_list.append(row)
-                return item_list
+            return item_list
     except FileNotFoundError:
         print("File not found, please try again.")
     if not item_list:
@@ -21,7 +21,6 @@ def load(file_name):
 
 
 def save_products(product_list):
-    # data hardcoded as model isn't finished
     try:
         with open(products_file_path, "w", newline="") as file:
             fieldnames = ["name", "price"]
@@ -29,7 +28,6 @@ def save_products(product_list):
             writer.writeheader()
             for product in product_list:
                 writer.writerow(product)
-            
     except FileNotFoundError:
         print("File not found, please try again.")
     except TypeError:
@@ -39,11 +37,6 @@ def save_products(product_list):
 
 
 def save_couriers(courier_list):
-    # hardcoded as model yet to be written
-    courier_list = [
-        {"name": "Bob", "phone_number": "0789887889"},
-        {"name": "Jane", "phone_number": "0783458075"},
-    ]
     try:
         with open(couriers_file_path, "w", newline="") as file:
             fieldnames = ["name", "phone_number"]
@@ -60,7 +53,6 @@ def save_couriers(courier_list):
 
 
 def save_orders(order_list):
-    
     try:
         with open(orders_file_path, "w", newline="") as file:
             fieldnames = [
@@ -69,14 +61,11 @@ def save_orders(order_list):
                 "customer_phone",
                 "courier",
                 "status",
-                "items",
-            ]
+                "items"]
             writer = csv.DictWriter(file, fieldnames=fieldnames)
             writer.writeheader()
             for order in order_list:
                 writer.writerow(order)
-            else:
-                print("Incorrect file extension. Please make sure file is csv.")
     except FileNotFoundError:
         print("File not found, please try again.")
     except TypeError:
