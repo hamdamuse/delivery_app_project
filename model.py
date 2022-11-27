1b8d7a0d07916ef64bd15d3fcc34dee1dc181bfd
Hamda Muse <hmuse300@gmail.com>
2022-11-27 13:11:55 +0000

    Include better input validation

--- model.py
+++ model.py
@@ -17,68 +17,24 @@ def load_products():
     ]
     return formatted_product_list
 
-
 product_list = load_products()
-
 
 def load_couriers():
     courier_list = load(couriers_file_path)
     return courier_list
 
-
 courier_list = load_couriers()
-
 
 def load_orders():
     order_list = load(orders_file_path)
     return order_list
 
-
 order_list = load_orders()
-
-
-def save_products_to_csv():
-    save_products(product_list)
-
-
-def save_couriers_to_csv():
-    save_couriers(courier_list)
-
-
-def save_orders_to_csv():
-    save_orders(order_list)
-
 
 def print_products(product_list):
     for product in product_list:
         print(f"Name: {product['name']}, Price: {product['price']:.2f}")
-
-
-
-def print_products_with_indexes(products):
-    print("\tProducts with their indexes:")
-    for index, product in enumerate(product_list):
-        print(f"""\tIndex: {index}, Name: {product['name']}, Price: {product['price']}""")
-
-
-
-def print_couriers_with_indexes(couriers):
-    print("\tCouriers with their indexes:")
-    for index, courier in enumerate(courier_list):
-        print(f"""Index: {index}, Name: {courier['name']}, Phone: {courier['phone_number']}""")
-
-def print_orders_with_indexes(orders):
-    print("\tOrders with their indexes:")
-    for index, order in enumerate(order_list):
-        print(f"""
-        Index: {index},
-        Customer name: {order['customer_name']},
-        Customer address: {order['customer_address']},
-        Customer phone{order['customer_phone']}, 
-        Courier: {order['courier']}
-        Status: {order['status']},
-        Items: {order['items']}
-        """)
+    
 
 
 
@@ -121,7 +77,7 @@ def add_order(order_list, customer_name, customer_address, customer_phone):
             """    
 Please enter indexes of products,separated by commas, to add to order -> """).replace(" ", ""))
     print_couriers_with_indexes(courier_list)
-    courier = input("Please enter index of courier to add to order -> ")
+    courier = input("Please enter index of courier to add to order -> ") #TODO: sort out user input type
     new_order = {
         "customer_name": customer_name,
         "customer_address": customer_address,
@@ -133,60 +89,97 @@ Please enter indexes of products,separated by commas, to add to order -> """).re
     order_list.append(new_order)
     return order_list
 
+def print_products_with_indexes(products):
+    print("\n\tProducts with their indexes:")
+    for index, product in enumerate(product_list):
+        print(f"""\tIndex: {index}, Name: {product['name']}, Price: {product['price']}""")
+
+
+def print_couriers_with_indexes(couriers):
+    print("\n\tCouriers with their indexes:")
+    for index, courier in enumerate(courier_list):
+        print(f"""Index: {index}, Name: {courier['name']}, Phone: {courier['phone_number']}""")
+
+
 def update_product(products):
     print_products_with_indexes(product_list)
-    while True: 
-        product_index = int(input("Please enter index of product to update -> "))
-        product_name = input("Please enter updated product name -> ")
-        product_price = input("Please enter updated product price -> ")
-        if product_name:
-            product_list[product_index]['name'] = product_name
-        if product_price:
-            try:
-                float(product_price)
-            except ValueError:
+    while True:
+        try: 
+            product_index = int(input("Please enter index of product to update -> "))
+            product_name = input("Please enter updated product name -> ").strip()
+            product_price = float(input("Please enter updated product price -> "))
+            if product_name:
+                product_list[product_index]['name'] = product_name
+            if product_price:
+                product_list[product_index]['price'] = product_price
+                return product_list
+        except ValueError:
                 print("Error: please enter a number")
                 continue
-    
-            product_list[product_index]['price'] = product_price
-            return product_list
     
 
 def update_courier(couriers):
     print_couriers_with_indexes(courier_list)   
     courier_index = int(input("Please enter index of courier to update -> "))
-    courier_name = input("Please enter updated courier name -> ")
-    courier_phone = input("Please enter updated courier phone ->")
+    courier_name = input("Please enter updated courier name -> ").strip()
+    courier_phone = input("Please enter updated courier phone ->").strip()
     if courier_name:
         courier_list[courier_index]['name'] = courier_name
     if courier_phone:
         courier_list[courier_index]['phone_number'] = courier_phone
     return product_list
 
+
 def update_order(orders):
-    print_orders_with_indexes(order_list)
-    order_index = int(input("Please enter index of order to update -> "))
-    customer_name = input("Please enter updated customer name -> ")
-    customer_address = input("Please enter updated customer address -> ")
-    print_couriers_with_indexes(courier_list)
-    courier = (input("Please enter updated courier index -> "))
-    try:
-        courier - int(courier)
-    except ValueError:
-        print("please enter a number ")
-    status = input("Please enter updated order status -> ")
-    items = input("Please enter updated product indices, separated by commas -> ").replace(" ", "")
-    if customer_name:
-        order_list[order_index]['customer_name'] = customer_name
-    if customer_address:
-        order_list[order_index]['customer_address'] = customer_address
-    if courier:
-        order_list[order_index]['courier'] = courier
-    if status:
-        order_list[order_index]['status'] = status
-    if items:
-        order_list[order_index]['items'] = items
-    return order_list
+    while True:
+        try:
+            print_orders_with_indexes(order_list)
+            order_index = int(input("Please enter index of order to update -> "))
+            customer_name = input("Please enter updated customer name -> ").strip()
+            customer_address = input("Please enter updated customer address -> ").strip()
+            print_couriers_with_indexes(courier_list)
+            courier = int(input("Please enter updated courier index -> "))
+            print_order_status_options_with_indexes()
+            order_status_index = int(input("Please enter index of updated order status-> "))
+        except ValueError:
+            print("please enter a number ")
+            continue
+        items = input("Please enter updated product indices, separated by commas -> ").replace(" ", "")
+        if customer_name:
+            order_list[order_index]['customer_name'] = customer_name
+        if customer_address:
+            order_list[order_index]['customer_address'] = customer_address
+        if courier:
+            order_list[order_index]['courier'] = courier
+        if order_status_index:
+            order_list[order_index]['status'] = order_status_list[order_status_index]
+        if items:
+            order_list[order_index]['items'] = items
+        return order_list
+
+
+def print_order_status_options_with_indexes():
+    order_statuses = ["PREPARING","OUT FOR DELIVERY","DELIVERED"]
+    for order_status in enumerate(order_statuses):
+        print("\n\t Order statuses with indexes:")
+        print(f"Index: {order_status}")
+    return order_statuses
+
+order_status_list = print_order_status_options_with_indexes()
+
+
+def print_orders_with_indexes(orders):
+    print("\tOrders with their indexes:")
+    for index, order in enumerate(order_list):
+        print(f"""
+        Index: {index},
+        Customer name: {order['customer_name']},
+        Customer address: {order['customer_address']},
+        Customer phone{order['customer_phone']}, 
+        Courier: {order['courier']}
+        Status: {order['status']},
+        Items: {order['items']}
+        """)
 
 
 def delete_product(products):
@@ -197,6 +190,7 @@ def delete_product(products):
     except ValueError:
         print("Please enter a number.")
 
+
 def delete_courier(couriers):
     print_couriers_with_indexes(courier_list)
     try:
@@ -204,6 +198,7 @@ def delete_courier(couriers):
         courier_list.pop(index)
     except ValueError:
         print("Please enter a number.")
+
 
 def delete_order(orders):
     print_orders_with_indexes(order_list)
@@ -214,4 +209,13 @@ def delete_order(orders):
         print("Please enter a number.")
 
 
+def save_products_to_csv():
+    save_products(product_list)
 
+
+def save_couriers_to_csv():
+    save_couriers(courier_list)
+
+
+def save_orders_to_csv():
+    save_orders(order_list)
